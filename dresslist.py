a9eaa279e5467af96af052c10606c862196d5d54
unknown <mcrucovschii@gmail.com>
2022-03-11 10:09:54 +0100

    ded

---
+++ dresslist.py
@@ -0,0 +1,9 @@
+# List
+myDressList = ['Skirt','Shorts',"D'ress"]
+print (myDressList)
+print (type(myDressList))
+print(myDressList[1])
+temp = myDressList[0]
+myDressList[0] = myDressList[1]
+myDressList[1] = temp
+print (myDressList)
